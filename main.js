e99d89303043929e74a7d093a1e41f6941a105bd
xxdevice <ttyejan@gmail.com>
2021-01-19 23:12:53 +0900

    hoge

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ $(function() {
 
     for(var i = 0; i < len; i++) {
       ulObj.append($("<li>").attr({"id":data[i].id}).text(data[i].weapon));
+      $("#weapons").append($("<option>").attr({"valude":i}).text(data[i].weapon));
       data[i].type.forEach(element => {
         ulObj.append($("<li>").attr({"id":data[i].id}).text(element.series));
       });
