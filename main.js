a98e8473b04239c851595869d0a070f738d915d0
xxdevice <ttyejan@gmail.com>
2021-01-20 09:05:44 +0900

    m

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ $(function() {
       len = data.length;
       wepons = data;
 
+    
     for(var i = 0; i < len; i++) {
-      ulObj.append($("<li>").attr({"id":data[i].id}).text(data[i].weapon));
+      $("#demo").append($("<li>").attr({"id":data[i].id}).text(data[i].weapon));
       $("#weapons").append($("<option>").attr({"valude":i}).text(data[i].weapon));
       data[i].type.forEach(element => {
-        ulObj.append($("<li>").attr({"id":data[i].id}).text(element.series));
+        $("#demo").append($("<li>").attr({"id":data[i].id}).text(element.series));
       });
     }
   });
