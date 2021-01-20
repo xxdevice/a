99c315e8b904dbc1a133cc8761aab9fa2459e8e0
xxdevice <ttyejan@gmail.com>
2021-01-20 12:04:03 +0900

    ...

--- main.js
+++ main.js
@@ -1,18 +1,56 @@
-
-var wepons = "";
+function search(weap,aff){
+  wept = weap.weapon;
+  if ( wept === "knife" || wept === "long" || wept === "dual" || wept === "javelin" || wept === "photon" ) {
+    wept = 0;
+  } else if ( wept === "shield" ){
+    wept = 1;
+  } else {
+    wept = 2;
+  }
+  return wept;
+}
+var weapons = "";
+var $ffix = "";
 $(function() {
-  
-  $.getJSON("weapons.json" , function(data) {
-    var
-      ulObj = $("#demo"),
-      len = data.length;
-      wepons = data;
-    console.log(len);
-    for(var i = 0; i < len; i++) {
-      $("#demo").append($("<li>").attr({"id":data[i].id}).text(data[i].weapon));
-      data[i].type.forEach(function(element,index){
-        $("#demo").append($("<li>").attr({"id":data[i].id}).text(element.series));
-      });
-    }
+  $.getJSON("affix.json" , function(affix) {
+    $.getJSON("weapons.json" , function(data) {
+      var
+        len = data.length;
+        weapons = data;
+        $affix = affix;
+      for(var i = 0; i < len; i++) {
+        wept = search(data[i],affix);
+        data[i].type.forEach(function(element,i){
+          switch(element.maker) {
+            case 'サクラバ重工':
+              index = 0;
+              break;
+            case 'グラナダ・GG':
+              index = 1;
+              break;
+            case 'メレデス＆コー':
+              index = 2;
+              break;
+            case '偽りなき真心堂':
+              index = 3;
+              break;
+            case 'ノポン商会':
+              index = 4;
+              break;
+            case 'オルフェ・パルフェ':
+              index = 5;
+              break;
+            case 'ファクトリー1.21':
+              index = 6;
+              break;
+            case '六連星':
+              index = 7;
+              break;
+          }
+          console.log(affix[wept].type[index]);
+          $("#sheat .page").append($('<div class="affix">').append("<table><caption>"+element.series+"</table></caption>").append($('<td>').append('<label><input type="checkbox"><span>射撃力アップ</span></label>')/* アフィクス*/));
+        });
+      }
+    });
   });
 });
