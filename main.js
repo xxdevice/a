4a6214b7c5839e54467d8b05d54f429a8a227577
xxdevice <ttyejan@gmail.com>
2021-01-20 10:19:52 +0900

    js test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$("#demo").append($("<li>").text("element.series"));
+
 var wepons = "";
 $(function() {
   
@@ -7,9 +7,10 @@ $(function() {
       ulObj = $("#demo"),
       len = data.length;
       wepons = data;
+    console.log(len);
     for(var i = 0; i < len; i++) {
       $("#demo").append($("<li>").attr({"id":data[i].id}).text(data[i].weapon));
-      data[i].type.forEach(function(element,i){
+      data[i].type.forEach(function(element,index){
         $("#demo").append($("<li>").attr({"id":data[i].id}).text(element.series));
       });
     }
