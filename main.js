8893b8524331c808283118a4e5c77a92fda37dd0
xxdevice <ttyejan@gmail.com>
2021-01-20 12:49:36 +0900

    affix

--- main.js
+++ main.js
@@ -20,36 +20,24 @@ $(function() {
         $affix = affix;
       for(var i = 0; i < len; i++) {
         wept = search(data[i],affix);
-        data[i].type.forEach(function(element,i){
+        data[i].type.forEach(function(element){
           switch(element.maker) {
-            case 'サクラバ重工':
-              index = 0;
-              break;
-            case 'グラナダ・GG':
-              index = 1;
-              break;
-            case 'メレデス＆コー':
-              index = 2;
-              break;
-            case '偽りなき真心堂':
-              index = 3;
-              break;
-            case 'ノポン商会':
-              index = 4;
-              break;
-            case 'オルフェ・パルフェ':
-              index = 5;
-              break;
-            case 'ファクトリー1.21':
-              index = 6;
-              break;
-            case '六連星':
-              index = 7;
-              break;
+            case 'サクラバ重工':index = 0;break;
+            case 'グラナダ・GG':index = 1;break;
+            case 'メレデス＆コー':index = 2;break;
+            case '偽りなき真心堂':index = 3;break;
+            case 'ノポン商会':index = 4;break;
+            case 'オルフェ・パルフェ':index = 5;break;
+            case 'ファクトリー1.21':index = 6;break;
+            case '六連星':index = 7;break;
           }
-          console.log(affix[wept].type[index]);
-          $("#sheat .page").append($('<div class="affix">').append("<table><caption>"+element.series+"</table></caption>").append($('<td>').append('<label><input type="checkbox"><span>射撃力アップ</span></label>')/* アフィクス*/));
+          aff = affix[wept].type[index];
+          $("#sheat .page").append($('<div class="affix">').append("<table><caption>"+element.series+"</table></caption>").append($('<td>').append(aff.affix.map(function(el,idx){
+            return '<label><input type="checkbox"><span>'+ el +'</span></label>';
+          }))));
+          console.log("----------");
         });
+        
       }
     });
   });
